3ba137a70b967077052fe5e2a061d521cdc32e67
Scott Guest <Scott.Guest@live.com>
2016-03-16 20:58:55 -0400

    Working image processing, autotarget does not work yet

--- src/com/github/adambots/stronghold2016/camera/Camera.java
+++ src/com/github/adambots/stronghold2016/camera/Camera.java
@@ -8,9 +8,15 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.*;
 
+import com.ni.vision.NIVision;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Camera {
+	
+	private static final int IMAGE_WRITE_ITERATIONS = 10;
+	
+	
 	public static final int IMAGE_HEIGHT = 240;
 	public static final int IMAGE_WIDTH = 320;
 	private static final int PROP_IMAGE_HEIGHT = 4;
@@ -18,18 +24,19 @@ public class Camera {
 	private static final int WEB_CAM = 0;
 	//HSV Filter constants
 	private static final int V_MAX = 255;
-	private static final int V_MIN = 250;
-	private static final int S_MAX = 13;
-	private static final int S_MIN = 0;
-	private static final int H_MAX = 81;
-	private static final int H_MIN = 55;
+	private static final int V_MIN = 200;
+	private static final int S_MAX = 255;
+	private static final int S_MIN = 140;
+	private static final int H_MAX = 180/180 * 255;
+	private static final int H_MIN = 87 / 180 * 255;
 
 	//Contour filtering constants
 	private static final double MIN_ASPECT_RATIO = 0;
-	private static final int MIN_CONTOUR_WIDTH = 100;
-	private static final int MIN_CONTOUR_HEIGHT = 100;
-	private static final int MAX_CONTOUR_WIDTH = 500;
-	private static final int MAX_CONTOUR_HEIGHT = 500;
+	private static final double MAX_ASPECT_RATIO = 0;
+	private static final int MIN_CONTOUR_WIDTH = 40;
+	private static final int MIN_CONTOUR_HEIGHT = 20;
+	private static final int MAX_CONTOUR_WIDTH = 70;
+	private static final int MAX_CONTOUR_HEIGHT = 100;
 	private static final int MAX_AREA = -1;
 	private static final int MIN_AREA = -1;
 
@@ -49,7 +56,7 @@ public class Camera {
 	private static Mat matOriginal, matHSV, matThresh, matHeirarchy;
 
 	//Camera stream
-	private static VideoCapture videoCapture;
+	public static VideoCapture videoCapture;
 
 	//	the height to the top of the target in first stronghold is 97 inches	
 	public static final int TOP_TARGET_HEIGHT = 97;
@@ -61,6 +68,8 @@ public class Camera {
 	public static final double VERTICAL_FOV  = 0;
 	public static final double HORIZONTAL_FOV  = 0;
 	public static final double CAMERA_ANGLE = 0;
+	
+	private static int iterator = 1;
 
 	
 	public static void init(){
@@ -83,14 +92,15 @@ public class Camera {
 		videoCapture.set(Videoio.CV_CAP_PROP_FRAME_WIDTH, IMAGE_WIDTH);
 		videoCapture.set(Videoio.CV_CAP_PROP_FRAME_HEIGHT, IMAGE_HEIGHT);
 //		videoCapture.set(Videoio.CV_CAP_PROP_XI_MANUAL_WB , 900);
-		videoCapture.set(Videoio.CAP_PROP_SATURATION, 50);
+		videoCapture.set(Videoio.CAP_PROP_SATURATION, 10);
+		videoCapture.set(Videoio.CAP_PROP_BRIGHTNESS, 0);
 //		videoCapture.set(Videoio.CAP_PROP_EXPOSURE, 10);
 		return videoCapture.isOpened();
 	}
 
 	public static boolean closeStream(){
 		videoCapture.release();
-		return videoCapture.isOpened();
+		return !videoCapture.isOpened();
 	}
 
 	/**
@@ -110,7 +120,6 @@ public class Camera {
 		System.out.println(target.getCenterX() + " "+ target.getCenterY());
 		System.out.println(target.getHeight());
 		System.out.println(target.getWidth());
-		System.out.println(closeStream());
 
 	}
 
@@ -128,28 +137,45 @@ public class Camera {
 			//TODO: TEST CODE---
 			Point center = new Point(rec.br().x-rec.width / 2 - 15,rec.br().y - rec.height / 2);
 			Point centerw = new Point(rec.br().x-rec.width / 2 - 15,rec.br().y - rec.height / 2 - 20);
-			Imgproc.putText(matOriginal, "HELLO WORLD", center, Core.FONT_HERSHEY_PLAIN, 1, RED);
+			Imgproc.putText(matOriginal, centerX + " : "+ centerY, center, Core.FONT_HERSHEY_PLAIN, 1, RED);
 			Imgproc.putText(matOriginal, "", centerw, Core.FONT_HERSHEY_PLAIN, 1, RED);
-			Imgcodecs.imwrite("/home/lvuser/output.png", matOriginal);
+			
+			if(IMAGE_WRITE_ITERATIONS % iterator == 0){
+				Imgcodecs.imwrite("/home/lvuser/output.png", matOriginal);
+			}
+			
 			//---
 			return new Target(centerX, centerY, rec.width * rec.height, distance, rec.height, rec.width);
-		}else{
-			return null;
 		}
+		
+		return new Target(-1, -1, -1, -1, -1, -1);
+		
 	}
 	
 	
 	public static ArrayList<MatOfPoint> getContours(){
 		// Grabs image from stream
-		videoCapture.read(matOriginal);
+//		Mat last = matOriginal;
+//		for(int i = 0; i < 1000 && !matOriginal.empty(); i++){
+//			last = matOriginal.clone();
+//			videoCapture.read(matOriginal);
+//		}
+//		matOriginal = last;
+		
+		for(int i = 0; i < 30; i++){
+			videoCapture.read(matOriginal);
+		}
+//		System.out.println("IS STREAM CLOSED?: "+closeStream());
+//		videoCapture = null;
 
 		//Converts image to HSV
 		Imgproc.cvtColor(matOriginal,matHSV,Imgproc.COLOR_RGB2HSV);
 		Core.inRange(matHSV, SCALAR_LOWER_BOUNDS, SCALAR_UPPER_BOUNDS, matThresh);
 		//TODO:TEST CODE---
-		Imgcodecs.imwrite("/home/lvuser/original.png", matOriginal);
-		Imgcodecs.imwrite("/home/lvuser/hsv.png", matHSV);
-		Imgcodecs.imwrite("/home/lvuser/thresh.png", matThresh);
+		if(IMAGE_WRITE_ITERATIONS % iterator == 0){
+			Imgcodecs.imwrite("/home/lvuser/original.png", matOriginal);
+			Imgcodecs.imwrite("/home/lvuser/thresh.png", matThresh);
+		}
 		//---
 		ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 		contours.clear();
@@ -160,7 +186,6 @@ public class Camera {
 		//		make sure the contours that are detected are at least 20x20 
 		//		pixels with an area of 400 and an aspect ration greater then 1
 		//TODO:TEST CODE---
-		Imgcodecs.imwrite("/home/lvuser/contours.png", matThresh);
 		//---
 		for (Iterator<MatOfPoint> iterator = contours.iterator(); iterator.hasNext();) {
 
@@ -178,13 +203,16 @@ public class Camera {
 			}
 		}
 		System.out.println("DONE WITH CONTOURS");
+		iterator++;
 		return contours;
 	}
+	
 	private static boolean isNotTarget(Rect rec){
 		boolean isTarget = rec.height < MIN_CONTOUR_HEIGHT || rec.width < MIN_CONTOUR_WIDTH;
 		isTarget = isTarget || rec.height > MAX_CONTOUR_HEIGHT || rec.width > MAX_CONTOUR_WIDTH;
 		return isTarget;
 	}
+	
 	public static MatOfPoint getBestContour() {
 
 		ArrayList<MatOfPoint> contours = getContours();
